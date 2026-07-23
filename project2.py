2e105a8064d4f4262bb71ef0e7de3bd2dbbf8131
Maryam01-coder <maryamokikiola@gmail.com>
2026-07-23 06:46:20 -0400

    latest_version

--- project2.py
+++ project2.py
@@ -1,65 +1,82 @@
 print("Welcome to your To-Do list app." )
 
-def add_task(filename):
-    try:
-        add_task = input("\nInput a task: ")
-        with open(filename, "a") as task_file:
-            text = task_file.write(add_task + "\n")
-        print(f"\nYour task '{add_task}' has successfully been added.")
-    
+task = []
+
+def save_tasks(all_tasks):
+    with open("tasks.txt", "w") as task_file:
+        for task in all_tasks:
+            task_file.write(f"{task['task']}||{task['status']}\n")
         
+def load_tasks(filename):
+    try: 
+        tasks_list =[]
+        with open(filename, "r") as task_file:
+            all_tasks = task_file.readlines()
+        for task in all_tasks:
+                task_parts = task.strip().split("||")
+
+                task_data = {"task": task_parts[0], "status": task_parts[1]}
+                tasks_list.append(task_data)
+        return tasks_list
+    
+    except FileNotFoundError:
+        print("The required file is not found in this directory")
+
+def add_task():
+    try:
+        add_task = input("\nInput your task: ").capitalize()
+
+        with open("tasks.txt", "a") as task_file:
+                task_file.write(f"{add_task}||Pending\n")
+
+        print(f"\nYour task '{add_task}' has successfully been added.")
 
     except FileNotFoundError:
         print("The required file is not found in this directory")
 
 def view_tasks(filename):
-    try: 
-        with open(filename, "r") as task_file:
-            tasks = task_file.read()
-            print(f"\n{tasks}")
-    
-    except FileNotFoundError:
-        print("The required file is not found in this directory")
+    all_tasks = load_tasks(filename)
+    print("-"*60)
+    print(f"|{'ID':<7}|{'Task':<34}|{'Status':<15}|")
+    print("-"*60)
+    for index, task in enumerate(all_tasks, start=1):
+        print(f"|{index:<7}|{task['task']:<34}|{task['status']:<15}|")
+    print("-"*60)
 
 def completed(filename):
+    view_tasks(filename)
+    all_tasks = load_tasks(filename)
     try:
-        task_num = int(input("\nWhat is the number of the task completed? "))
+        task_num = int(input("\nEnter the number of the completed task? "))
 
-        with open(filename, "r") as task_file:
-            all_tasks = task_file.readlines() 
-            task_index = task_num - 1 
+        task_index = task_num - 1 
 
-            if task_index < len(all_tasks):
-                old_task = all_tasks[task_index].strip()
+        if 0 <= task_index < len(all_tasks):
+            old_task = all_tasks[task_index]["task"]
+            all_tasks[task_index]["status"] = "Completed"
 
-                new_task = f"{old_task} - Completed\n"
-                all_tasks[task_index] = new_task
-
-                print(f"\nThe task '{old_task}' has been completed.")
-
-                with open(filename, "w") as task_file:
-                    task_file.writelines(all_tasks)
-            else: 
-                print("Please enter a valid number.")
+            save_tasks(all_tasks)
+            print(f"\nThe task '{old_task}' has been completed.")
+           
+        else: 
+            print("Please enter a valid number.")
 
     except ValueError:
         print("\nThe number inputted is not in the list.")
 
 def delete_task(filename):
+    view_tasks(filename)
     try:
+        all_tasks = load_tasks(filename)
         task_num = int(input("\nWhat is the number of the task you want to delete? "))
     
         task_index = task_num - 1
 
-        with open(filename, "r") as file:
-            all_tasks = file.readlines()
-
-        if task_index < len(all_tasks):
+        if 0 <= task_index < len(all_tasks):
             removed_task = all_tasks.pop(task_index)
-            print(f"\n'{removed_task.strip()}' has successfully being removed from your task list.")
-            
-            with open(filename, 'w') as file:
-                file.writelines(all_tasks)
+            print(f"\n'{removed_task['task']}' has successfully being removed from your task list.")
+  
+            save_tasks(all_tasks)
         else:
             print("The number entered is not in the list.")
     except ValueError:
@@ -70,7 +87,7 @@ while True:
     ops = input("\nEnter any of the following options('add', 'view', 'completed', 'delete', 'exit': ").strip().lower()
 
     if ops == "add":
-        add_task("tasks.txt")
+        add_task()
     elif ops =="view":
         view_tasks("tasks.txt")
     elif ops == "completed":
